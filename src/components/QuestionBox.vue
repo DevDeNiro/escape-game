3c913aa050bb38ea46f47a8afd8ad9e07bacf18a
Noodles-boops <Oldoni.jeremy@outlook.fr>
2023-10-12 11:25:06 +0200

    + speed dialog

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,54 +1,58 @@
 <template>
-	<div class="room flex justify-center items-center min-h-screen bg-blue-200">
-		<Box>
-			<template v-if="hasNoMoreSituation">
-				<p>
-					Félicitations. Vous avez terminé la Room n°{{currentRoomId}}.
-					Vous obtenez une clé pour accéder à la room suivante.
-				</p>
-				<div class="flex items-center gap-3">
-					<ButtonComponent
-						classes="flex-1 h-full"
-						v-if="currentRoomId + 1 >= roomsCount"
-						:action="onEnd"
-					>
-						Terminer l'exercice
-					</ButtonComponent>
+  <div class="room flex justify-center items-center min-h-screen bg-blue-200">
+    <Box>
+      <template v-if="hasNoMoreSituation">
+        <p>
+          Félicitations. Vous avez terminé la Room n°{{ currentRoomId }}. Vous
+          obtenez une clé pour accéder à la room suivante.
+        </p>
+        <div class="flex items-center gap-3">
+          <ButtonComponent
+            classes="flex-1 h-full"
+            v-if="currentRoomId + 1 >= roomsCount"
+            :action="onEnd"
+          >
+            Terminer l'exercice
+          </ButtonComponent>
 
-					<template v-else>
-						<ButtonComponent
-							classes="flex-1 h-full"
-							:action="onNextRoom"
-							v-if="percentage >= 50"
-						>
-							Room suivante
-						</ButtonComponent>
-						<ButtonComponent classes="flex-1 h-full" v-else>
-							Recommencer
-						</ButtonComponent>
-					</template>
+          <template v-else>
+            <ButtonComponent
+              classes="flex-1 h-full"
+              :action="onNextRoom"
+              v-if="percentage >= 50"
+            >
+              Room suivante
+            </ButtonComponent>
+            <ButtonComponent classes="flex-1 h-full" v-else>
+              Recommencer
+            </ButtonComponent>
+          </template>
 
-					<ButtonComponent
-						classes="flex-1 h-full"
-						:is-secondary="true"
-						:action="() => isResultVisible = !isResultVisible"
-					>
-						Voir les résultats {{ isResultVisible ? "▲" : "▼"}}
-					</ButtonComponent>
-				</div>
-				<result-display
-					v-show="isResultVisible"
-					:percentage="percentage"
-					:current-room-id="currentRoomId"
-				/>
-			</template>
-			<template v-else>
-				<h2 class="text-lg font-bold w-fit bg-black text-white rounded-full py-1 px-3 mx-auto">{{ theme }}</h2>
-				<p class="text-center">{{ situation.scenario }}</p>
-				<slot></slot>
-			</template>
-		</Box>
-	</div>
+          <ButtonComponent
+            classes="flex-1 h-full"
+            :is-secondary="true"
+            :action="() => (isResultVisible = !isResultVisible)"
+          >
+            Voir les résultats {{ isResultVisible ? "▲" : "▼" }}
+          </ButtonComponent>
+        </div>
+        <result-display
+          v-show="isResultVisible"
+          :percentage="percentage"
+          :current-room-id="currentRoomId"
+        />
+      </template>
+      <template v-else>
+        <h2
+          class="text-lg font-bold w-fit bg-black text-white rounded-full py-1 px-3 mx-auto"
+        >
+          {{ theme }}
+        </h2>
+        <p class="text-center">{{ situation.scenario }}</p>
+        <slot></slot>
+      </template>
+    </Box>
+  </div>
 </template>
 
 <script>
@@ -61,40 +65,42 @@ import ResultDisplay from "@/components/ResultDisplay.vue";
 import {useRouter} from "vue-router";
 
 export default {
-	components: {ResultDisplay, Answers, Box, ButtonComponent},
-	props: {
-		situation: Object,
-		theme: String,
-		hasNoMoreSituation: Boolean,
-		onNextRoom: Function,
-		roomAnswers: Array
-	},
-	setup(props) {
-		const {currentRoomId, roomsCount} = useRoomStore()
-		const isResultVisible = ref(false);
-		const router = useRouter();
-		const percentage = computed(() => {
-			const result = props.roomAnswers
-				.reduce((acc, answer) => {
-					if (answer.isCorrect) acc++;
-					return acc;
-				}, 0) / props.roomAnswers.length * 100;
-			return result.toFixed(2);
-		})
+  components: {ResultDisplay, Answers, Box, ButtonComponent},
+  props: {
+    situation: Object,
+    theme: String,
+    hasNoMoreSituation: Boolean,
+    onNextRoom: Function,
+    roomAnswers: Array,
+  },
+  setup(props) {
+    const {currentRoomId, roomsCount} = useRoomStore();
+    const isResultVisible = ref(false);
+    const router = useRouter();
+    const percentage = computed(() => {
+      const result =
+        (props.roomAnswers.reduce((acc, answer) => {
+          if (answer.isCorrect) acc++;
+          return acc;
+        }, 0) /
+          props.roomAnswers.length) *
+        100;
+      return result.toFixed(2);
+    });
 
-		const onEnd = () => {
-			console.log("end");
-			router.push({name: "Results"})
-		};
+    const onEnd = () => {
+      console.log("end");
+      router.push({name: "Results"});
+    };
 
-		return {
-			isResultVisible,
-			percentage,
-			currentRoomId,
-			roomsCount,
-			onEnd
-		};
-	},
+    return {
+      isResultVisible,
+      percentage,
+      currentRoomId,
+      roomsCount,
+      onEnd,
+    };
+  },
 };
 </script>
 
--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,37 +1,38 @@
 <template>
-	<div class="room" v-if="currentRoom">
-		<div class="background"
-			 :style="{
-				backgroundImage: `url(${currentSituation ? imgFolder + currentSituation.background : ''})`
-			 }"
-		>
-			<img
-				:src="currentSituation ? imgFolder + currentSituation.sprite : ''"
-				alt="sprite"
-				class="sprite"
-				v-if="showDialog"
-			/>
-		</div>
-		<DialogBox
-			:message="currentMessage"
-			@next="handleNext"
-			v-if="showDialog"
-			:dialogues="currentRoom.dialog"
-			:no-more-dialog-action="noMoreDialogAction" />
-		<QuestionBox
-			:theme="currentRoom.theme"
-			:situation="currentSituation"
-			:has-no-more-situation="hasNoMoreSituation"
-			@next-room="nextRoom"
-			:room-answers="roomAnswers"
-			v-else
-		>
-			<Answers
-				:situation="currentSituation"
-				@answer="handleAnswer"
-			/>
-		</QuestionBox>
-	</div>
+  <div class="room" v-if="currentRoom">
+    <div
+      class="background"
+      :style="{
+        backgroundImage: `url(${
+          currentSituation ? imgFolder + currentSituation.background : ''
+        })`,
+      }"
+    >
+      <img
+        :src="currentSituation ? imgFolder + currentSituation.sprite : ''"
+        alt="sprite"
+        class="sprite"
+        v-if="showDialog"
+      />
+    </div>
+    <DialogBox
+      :message="currentMessage"
+      @next="handleNext"
+      v-if="showDialog"
+      :dialogues="currentRoom.dialog"
+      :no-more-dialog-action="noMoreDialogAction"
+    />
+    <QuestionBox
+      :theme="currentRoom.theme"
+      :situation="currentSituation"
+      :has-no-more-situation="hasNoMoreSituation"
+      @next-room="nextRoom"
+      :room-answers="roomAnswers"
+      v-else
+    >
+      <Answers :situation="currentSituation" @answer="handleAnswer" />
+    </QuestionBox>
+  </div>
 </template>
 
 <script>
@@ -44,102 +45,116 @@ import {useRoute, useRouter} from "vue-router";
 import Answers from "@/components/Answers.vue";
 
 export default {
-	components: {
-		Answers,
-		DialogBox,
-		QuestionBox,
-	},
-	setup() {
-		const {currentRoomId, currentRoom, addAnswer, userAnswers, setCurrentRoomId} = useRoomStore()
-		const route = useRoute();
-		const router = useRouter();
-		const paramId = route.params.id;
-		const showDialog = ref(true);
-		const currentMessage = ref("Chargement...");
-		const imgFolder = ref("")
-		const currentSituationId = ref(0);
+  components: {
+    Answers,
+    DialogBox,
+    QuestionBox,
+  },
+  setup() {
+    const {
+      currentRoomId,
+      currentRoom,
+      addAnswer,
+      userAnswers,
+      setCurrentRoomId,
+    } = useRoomStore();
+    const route = useRoute();
+    const router = useRouter();
+    const paramId = route.params.id;
+    const showDialog = ref(true);
+    const currentMessage = ref("Chargement...");
+    const imgFolder = ref("");
+    const currentSituationId = ref(0);
 
-		const currentSituation = computed(() => {
-			return currentRoom(paramId).situations[currentSituationId.value];
-		});
-		const roomAnswers = computed(() => {
-			return userAnswers.filter((answer) => Number(answer.roomId) === Number(currentRoomId));
-		})
-		const hasNoMoreSituation =  computed(() => {
-			return roomAnswers.value.length >= currentRoom(currentRoomId).situations.length;
-		});
+    const currentSituation = computed(() => {
+      return currentRoom(paramId).situations[currentSituationId.value];
+    });
+    const roomAnswers = computed(() => {
+      return userAnswers.filter(
+        (answer) => Number(answer.roomId) === Number(currentRoomId)
+      );
+    });
+    const hasNoMoreSituation = computed(() => {
+      return (
+        roomAnswers.value.length >= currentRoom(currentRoomId).situations.length
+      );
+    });
 
-		onMounted(() => {
-			try {
-				imgFolder.value = jsonData.imagesFolder;
-				currentMessage.value = "Bienvenue dans cette room !";
-			} catch (err) {
-				console.error("Erreur lors de la manipulation des données:", err);
-				currentMessage.value = "Erreur lors de la manipulation des données.";
-			}
-		});
+    onMounted(() => {
+      try {
+        imgFolder.value = jsonData.imagesFolder;
+        currentMessage.value = "Bienvenue dans cette room !";
+      } catch (err) {
+        console.error("Erreur lors de la manipulation des données:", err);
+        currentMessage.value = "Erreur lors de la manipulation des données.";
+      }
+    });
 
-		const handleNext = () => {
-			showDialog.value = false;
-		};
+    const handleNext = () => {
+      showDialog.value = false;
+    };
 
-		const handleAnswer = (index) => {
-			console.log("index de la réponse cliquée", index, currentSituation.value.responses[index - 1])
-			if (!hasNoMoreSituation.value) {
-				addAnswer({
-					roomId: currentRoomId,
-					situationId: currentSituation.value.id,
-					answerId: index - 1,
-					isCorrect: currentSituation.value.responses[index - 1].is_correct,
-				})
-				console.log(currentRoomId, currentRoom(paramId).situations.length)
-				currentSituationId.value++;
-			}
-		};
+    const handleAnswer = (index) => {
+      console.log(
+        "index de la réponse cliquée",
+        index,
+        currentSituation.value.responses[index - 1]
+      );
+      if (!hasNoMoreSituation.value) {
+        addAnswer({
+          roomId: currentRoomId,
+          situationId: currentSituation.value.id,
+          answerId: index - 1,
+          isCorrect: currentSituation.value.responses[index - 1].is_correct,
+        });
+        console.log(currentRoomId, currentRoom(paramId).situations.length);
+        currentSituationId.value++;
+      }
+    };
 
-		const nextRoom = () => {
-			setCurrentRoomId(Number(paramId) + 1)
-			console.log("test goToNextSituation() nextRoom")
-			currentSituationId.value = 0;
-			const nextId = Number(paramId) + 1
-			router.push("/room/" + nextId)
-		};
+    const nextRoom = () => {
+      setCurrentRoomId(Number(paramId) + 1);
+      console.log("test goToNextSituation() nextRoom");
+      currentSituationId.value = 0;
+      const nextId = Number(paramId) + 1;
+      router.push("/room/" + nextId);
+    };
 
-		const noMoreDialogAction = () => {
-			showDialog.value = false;
-		};
+    const noMoreDialogAction = () => {
+      showDialog.value = false;
+    };
 
-		return {
-			imgFolder,
-			currentRoom: currentRoom(paramId),
-			roomAnswers,
-			currentMessage,
-			currentSituation,
-			hasNoMoreSituation,
-			showDialog,
-			nextRoom,
-			handleNext,
-			handleAnswer,
-			noMoreDialogAction,
-		};
-	},
+    return {
+      imgFolder,
+      currentRoom: currentRoom(paramId),
+      roomAnswers,
+      currentMessage,
+      currentSituation,
+      hasNoMoreSituation,
+      showDialog,
+      nextRoom,
+      handleNext,
+      handleAnswer,
+      noMoreDialogAction,
+    };
+  },
 };
 </script>
 
 <style scoped>
 .background {
-	position: absolute;
-	width: 100%;
-	height: 100%;
-	background-size: cover;
-	background-position: center;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
 }
 
 .sprite {
-	position: absolute;
-	height: 300px;
-	bottom: 150px;
-	left: 50%;
-	transform: translateX(-50%);
+  position: absolute;
+  height: 300px;
+  bottom: 150px;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
